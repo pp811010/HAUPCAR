5f5bcd9003ec9ddd2d2d23856d3b6d60599965b0
pp811010 <pp811010@gmail.com>
2026-08-04 10:18:37 +0700

    setup tailwindCSS  and install shadcn ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-red-400'>test</p>
+    </>
+  )
+}
+
+export default App
